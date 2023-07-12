7227dfa28b3a32c00e8437392cd7b0d25b45e118
paulclindo <paulclindo@gmail.com>
2023-07-12 17:57:01 -0500

    fix webpack issues

--- packages/actor-webpage/webpack.config.js
+++ packages/actor-webpage/webpack.config.js
@@ -1,6 +1,11 @@
 const path = require("path"); // eslint-disable-line @typescript-eslint/no-var-requires
 const webpack = require("webpack"); // eslint-disable-line @typescript-eslint/no-var-requires
 const tailwindcss = require("tailwindcss"); // eslint-disable-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config({ path: "./.env" });
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -30,10 +35,14 @@ module.exports = {
     topLevelAwait: true,
   },
   plugins: [
-    tailwindcss, new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    tailwindcss,
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        'WSC_ORACLE': JSON.stringify(process.env.REACT_APP_WSC_ORACLE),
+      "process.env": {
+        WSC_ORACLE: JSON.stringify(process.env.REACT_APP_WSC_ORACLE),
       },
     }),
   ],
