a347f1836e745ca255cce5eecff26e228644d3ed
paulclindo <paulclindo@gmail.com>
2023-08-09 13:22:34 -0500

    improvements WSC example dapp

--- packages/milkomeda-wsc-ui-next-example/pages/index.tsx
+++ packages/milkomeda-wsc-ui-next-example/pages/index.tsx
@@ -7,36 +7,13 @@ import type { TransactionConfigWSCOptions } from "milkomeda-wsc-ui";
 import djedABI from "../abi/djed.json";
 import { ethers } from "ethers";
 import Link from "next/link";
+import { useAccount } from "wagmi";
 
 const DJED_ADDRESS = "0xc4c0669ea7bff70a6cfa5905a0ba487fc181dc37";
 
 const reserveCoinAddress = "0x66c34c454f8089820c44e0785ee9635c425c9128";
 // const cardanoAddressTReserveCoin =
 //   "cc53696f7d40c96f2bca9e2e8fe31905d8207c4106f326f417ec36727452657365727665436f696e";
-
-const account = "0xb449b3B9943b57F50bEc4E2C6FF861353490Afdb";
-
-const buyOptions: TransactionConfigWSCOptions = {
-  defaultWrapToken: {
-    unit: "lovelace",
-    amount: "10178117048345515637", // 10.17 mADA -> 10 MOR
-  },
-  defaultUnwrapToken: {
-    unit: reserveCoinAddress,
-    amount: "10000000", // amountUnscaled
-  },
-  titleModal: "Buy Reserve Coin",
-  evmTokenAddress: reserveCoinAddress,
-  evmContractRequest: {
-    address: DJED_ADDRESS,
-    abi: djedABI.abi as any,
-    functionName: "buyReserveCoins", //account, FEE_UI_UNSCALED, UI
-    args: [account, "0000000000000000000000000", "0x0232556C83791b8291E9b23BfEa7d67405Bd9839"],
-    overrides: {
-      value: ethers.BigNumber.from("10178117048345515637"),
-    },
-  },
-};
 
 // const sellOptions: TransactionConfigWSCOptions = {
 //   defaultWrapToken: {
@@ -68,6 +45,30 @@ const buyOptions: TransactionConfigWSCOptions = {
 
 const Home: NextPage = () => {
   const { isWSCConnected } = useWSCProvider();
+  const { address: account } = useAccount();
+
+  const buyOptions: TransactionConfigWSCOptions = {
+    defaultWrapToken: {
+      unit: "lovelace",
+      amount: "10178117048345515637", // 10.17 mADA -> 10 MOR
+    },
+    defaultUnwrapToken: {
+      unit: reserveCoinAddress,
+      amount: "10000000", // amountUnscaled
+    },
+    titleModal: "Buy Reserve Coin",
+    evmTokenAddress: reserveCoinAddress,
+    evmContractRequest: {
+      address: DJED_ADDRESS,
+      abi: djedABI.abi as any,
+      functionName: "buyReserveCoins", //account, FEE_UI_UNSCALED, UI
+      args: [account, "0000000000000000000000000", "0x0232556C83791b8291E9b23BfEa7d67405Bd9839"],
+      overrides: {
+        value: ethers.BigNumber.from("10178117048345515637"),
+      },
+    },
+  };
+
   return (
     <div
       style={{
