0fe13d8e39930d0ed121f065610fc6ce9e171cab
rinor <rinorhoxha@gmail.com>
2023-09-14 11:27:43 +0000

    chore(oracle): use ethers.providers.StaticJsonRpcProvider

--- packages/oracle/src/services/blockchain.service.ts
+++ packages/oracle/src/services/blockchain.service.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 import config from "../config";
 
-export const provider = new ethers.providers.JsonRpcProvider(config.jsonRpcProviderUrl);
+export const provider = new ethers.providers.StaticJsonRpcProvider(config.jsonRpcProviderUrl);
 
 export const wallet = new ethers.Wallet(config.signerPrivateKey, provider);
